40fa6949fcf2ccaf4ee439fd499f1ee261ad3a25
William Pilger <40075505+williampilger@users.noreply.github.com>
2025-05-20 09:46:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,7 +10,7 @@ then
     then
         sudo pacman -S --noconfirm python-nautilus
     else
-        echo "python-nautilus is already installed"
+        echo "python-nautilus já está instalado"
     fi
 elif type "apt-get" > /dev/null 2>&1
 then
@@ -28,7 +28,7 @@ then
     then
         sudo apt-get install -y $package_name
     else
-        echo "$package_name is already installed."
+        echo "$package_name já está instalado."
     fi
 elif type "dnf" > /dev/null 2>&1
 then
@@ -37,24 +37,24 @@ then
     then
         sudo dnf install -y nautilus-python
     else
-        echo "nautilus-python is already installed."
+        echo "nautilus-python já está instalado."
     fi
 else
-    echo "Failed to find python-nautilus, please install it manually."
+    echo "Não foi possível localizar o python-nautilus, por favor instale-o manualmente."
 fi
 
 # Remove previous version and setup folder
-echo "Removing previous version (if found)..."
+echo "Removendo a versão anterior (se houver)..."
 mkdir -p ~/.local/share/nautilus-python/extensions
 rm -f ~/.local/share/nautilus-python/extensions/VSCodeExtension.py
 rm -f ~/.local/share/nautilus-python/extensions/code-nautilus.py
 
 # Download and install the extension
-echo "Downloading newest version..."
-wget -q -O ~/.local/share/nautilus-python/extensions/code-nautilus.py https://raw.githubusercontent.com/harry-cpp/code-nautilus/master/code-nautilus.py
+echo "Baixando a nova versão..."
+wget -q -O ~/.local/share/nautilus-python/extensions/code-nautilus.py https://raw.githubusercontent.com/williampilger/code-nautilus/master/code-nautilus.py
 
 # Restart nautilus
-echo "Restarting nautilus..."
+echo "Reiniciando o nautilus..."
 nautilus -q
 
-echo "Installation Complete"
+echo "Instalação Completa"
